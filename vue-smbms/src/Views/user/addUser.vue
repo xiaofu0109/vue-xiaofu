9d7db9fce05fff77efbd905f1c2f331942f64fc6
xiaofu <59732212@qq.com>
2020-08-08 23:05:14 +0800

    添加用户页面修改

--- vue-smbms/src/Views/user/addUser.vue
+++ vue-smbms/src/Views/user/addUser.vue
@@ -1,18 +1,56 @@
 <template>
   <div id="addUser">
     <div id="forms">
-      <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-        <el-form-item label="密码" prop="pass">
-          <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+      <el-form :model="ruleForm" status-icon :rules="rules" ref="aa" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="用户编码" prop="usercode">
+          <el-input v-model="ruleForm.usercode"></el-input>
         </el-form-item>
-        <el-form-item label="确认密码" prop="checkPass">
-          <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+        <el-form-item label="用户名称" prop="username">
+          <el-input v-model="ruleForm.username"></el-input>
         </el-form-item>
-        <el-form-item label="年龄" prop="age">
-          <el-input v-model.number="ruleForm.age"></el-input>
+        <el-form-item label="密码" prop="userpassword">
+          <el-input type="password" v-model="ruleForm.userpassword" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="password">
+          <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="用户性别">
+          <el-select v-model="ruleForm.gender" placeholder="请选择">
+            <el-option
+              v-for="item in sex"
+              :key="item.id"
+              :label="item.value"
+              :value="item.id">
+            </el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="出生日期">
+          <el-date-picker
+            v-model="ruleForm.birthday"
+            type="date"
+            placeholder="选择日期">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="用户电话">
+          <el-input v-model="ruleForm.phone"></el-input>
+        </el-form-item>
+
+        <el-form-item label="用户地址">
+          <el-input v-model="ruleForm.address"></el-input>
+        </el-form-item>
+        <el-form-item label="用户角色">
+          <el-select v-model="ruleForm.userrole" placeholder="请选择">
+            <el-option
+              v-for="item in userroles"
+              :key="item.id"
+              :label="item.rolename"
+              :value="item.id">
+            </el-option>
+          </el-select>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+          <el-button type="primary" @click="submitForm('aa')">提交</el-button>
           <el-button @click="resetForm('ruleForm')">重置</el-button>
         </el-form-item>
       </el-form>
@@ -24,28 +62,12 @@
   export default {
     name: "add-user",
     data() {
-      var checkAge = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error('年龄不能为空'));
-        }
-        setTimeout(() => {
-          if (!Number.isInteger(value)) {
-            callback(new Error('请输入数字值'));
-          } else {
-            if (value < 18) {
-              callback(new Error('必须年满18岁'));
-            } else {
-              callback();
-            }
-          }
-        }, 1000);
-      };
       var validatePass = (rule, value, callback) => {
         if (value === '') {
           callback(new Error('请输入密码'));
         } else {
-          if (this.ruleForm.checkPass !== '') {
-            this.$refs.ruleForm.validateField('checkPass');
+          if (this.ruleForm.password !== '') {
+            this.$refs.aa.validateField('password');
           }
           callback();
         }
@@ -53,44 +75,81 @@
       var validatePass2 = (rule, value, callback) => {
         if (value === '') {
           callback(new Error('请再次输入密码'));
-        } else if (value !== this.ruleForm.pass) {
+        } else if (value !== this.ruleForm.userpassword) {
           callback(new Error('两次输入密码不一致!'));
         } else {
           callback();
         }
       };
+      var usercodevalidate = (rule,value,callback)=>{
+        if(value === ''){
+          callback(new Error('请输入用户编码'))
+        }
+        callback();
+      };
+      var usernamevalidate =(rule,value,callback) =>{
+        if(value == ''){
+          callback(new Error('请输入用户名称'));
+        }
+        callback();
+      }
       return {
         ruleForm: {
-          pass: '',
-          checkPass: '',
-          age: ''
+          usercode: '',
+          username: '',
+          userpassword: '',
+          password: '',
+          gender: '',
+          birthday: '',
+          phone: '',
+          address: '',
+          userrole: '',
+
         },
+        sex:[
+          {id:1,value:'男'},
+          {id:2,value:'女'}
+        ],
+        userroles:[''],
         rules: {
-          pass: [
+          userpassword: [
             {validator: validatePass, trigger: 'blur'}
           ],
-          checkPass: [
+          password: [
             {validator: validatePass2, trigger: 'blur'}
           ],
-          age: [
-            {validator: checkAge, trigger: 'blur'}
+          usercode:[
+            {validator:usercodevalidate,trigger:'blur'}
+          ],
+          username:[
+            {validator:usernamevalidate,trigger:'blur'}
           ]
         }
       };
     },
+    mounted(){
+      this.getRoles();
+    },
     methods: {
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert('submit!');
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
+      getRoles:function () {
+        const that = this;
+        this.$axios({
+          method:"post",
+          url:"http://localhost:8001/role/getlist"
+        }).then(function (res) {
+          that.userroles = res.data.data;
+        }).catch(function (error) {
+          alert(error);
+        })
       },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
+      submitForm:function (forms) {
+        this.$refs[forms].validate((valid) => {
+          if(valid){
+            alert('submit');
+          }else{
+            alert('error');
+          }
+        })
       }
     }
   }
